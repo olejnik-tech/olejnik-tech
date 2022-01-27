497235e3c046dfe33bba78e45595afc4b202416c
olejnik-tech <marek@olejnik.tech>
2022-01-27 14:09:08 -0500

    portfolio > downloading CV 'fixed' -> temporarly disabled with alert + a little styling

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -9,6 +9,11 @@ export class PortfolioComponent implements OnInit {
 
   constructor() { }
 
+  public downloadCV(){
+    console.log('downloading CV');
+    alert('Downloading CV is not supported, until I will solve the security dilema. Contact me, if you require it. Thanks for understanding.');
+  }
+
   ngOnInit(): void {
   }
 
