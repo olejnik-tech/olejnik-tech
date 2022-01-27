4d0aef63f95b5a4ea76fbcce1aae9148d6f852bb
olejnik-tech <marek@olejnik.tech>
2022-01-27 12:48:40 -0500

    first live version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BusinessCardComponent } from './business-card/business-card.component';
 import { HomeComponent } from './home/home.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'portfolio', component: PortfolioComponent}
+  {path: 'portfolio', component: PortfolioComponent},
+  {path: 'business-card', component: BusinessCardComponent}
 ];
 
 @NgModule({
